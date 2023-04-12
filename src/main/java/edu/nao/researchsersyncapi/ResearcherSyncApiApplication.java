1a49af8724a2044c40223d9acb708503e40b1973
Alain Bonilla <Alain.Bonilla@jda.com>
2023-04-12 15:40:07 -0600

    Add initial setup files for researchers syncrhonizer project

---
+++ src/main/java/edu/nao/researchsersyncapi/ResearcherSyncApiApplication.java
@@ -0,0 +1,13 @@
+package edu.nao.researchsersyncapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearcherSyncApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearcherSyncApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/nao/researchsersyncapi/ResearcherSyncApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.nao.researchsersyncapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearcherSyncApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
